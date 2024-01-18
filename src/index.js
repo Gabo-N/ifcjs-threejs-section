43afb5f5b05793898141b28358462f711fdc1939
Gabo-N <gabriel.nachman@gmail.com>
2024-01-18 16:48:34 -0300

    Add scene files

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import {
+    Scene,
+    BoxGeometry,
+    MeshBasicMaterial,
+    Mesh,
+    PerspectiveCamera,
+    WebGLRenderer
+} from 'three';
+
+// 1 The scene
+const scene = new Scene();
+
+// 2 The object
+const geometry = new BoxGeometry(0.5, 0.5, 0.5);
+const material = new MeshBasicMaterial({color: 'orange'});
+const mesh = new Mesh(geometry, material);
+
+scene.add(mesh)
+
+// 3 The camera
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+const camera = new PerspectiveCamera(75, sizes.width/sizes.height);
+scene.add(camera);
+camera.position.z = 3;
+
+// 4 The renderer
+const canvas = document.getElementById('three-canvas');
+const renderer = new WebGLRenderer( { canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
