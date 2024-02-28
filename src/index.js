1bb0e8e1d57a59b61661215e78a9547d8da8c9e1
Gabo-N <gabriel.nachman@gmail.com>
2024-02-28 18:36:38 -0300

    Add GLTF loader

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ import CameraControls from 'camera-controls';
 
 import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import gsap from 'gsap';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // 1 The scene
 const scene = new Scene();
@@ -48,92 +49,41 @@ axes.renderOrder = 2;
 scene.add(axes);
 
 const grid = new GridHelper();
-grid.material.depthTest = false;
+grid.renderOrder = 1;
 scene.add(grid);
 
 // 2 The objects
 
-const loader = new TextureLoader();
+const loader = new GLTFLoader();
 
-const radius = 0.5;
-const widthSegments = 12;
-const heightSegments = 8;
-const geometry = new SphereGeometry(radius, widthSegments, heightSegments);
+const loadingElem = document.querySelector('#loader-container');
+const loadingText = loadingElem.querySelector('p');
 
-const sunMaterial = new MeshLambertMaterial({
-    color: 'yellow'
-});
-const earthMaterial = new MeshLambertMaterial({
-    color: 'blue',
-    polygonOffset: true,
-    polygonOffsetFactor: 1, 
-    polygonOffsetUnits: 1
-});
-const moonMaterial = new MeshLambertMaterial({
-    color: 'white',
-    polygonOffset: true,
-    polygonOffsetFactor: 1, 
-    polygonOffsetUnits: 1
-});
+loader.load('../assets/police_station.glb',
 
-const sunMesh = new Mesh(geometry, sunMaterial);
-scene.add(sunMesh);
+	( gltf ) => {
+    loadingElem.style.display = 'none';
+		scene.add( gltf.scene );
+	},
 
-const earthMesh = new Mesh(geometry, earthMaterial);
-earthMesh.scale.set(.2, .2, .2);
-earthMesh.position.x += 2;
-sunMesh.add(earthMesh);
+	( progress ) => {
+    const current = (progress.loaded /  progress.total) * 100;
+    const formatted = Math.trunc(current * 100) / 100; 
+    loadingText.textContent = `Loading: ${formatted}%`;
+	},
 
-const moonMesh = new Mesh(geometry, moonMaterial);
-moonMesh.scale.set(.4, .4, .4);
-moonMesh.position.x += 1;
-earthMesh.add(moonMesh);
+	( error ) => {
 
-const earthAxes = new AxesHelper();
-earthAxes.material.depthTest = false;
-earthAxes.renderOrder = 2;
-earthMesh.add(earthAxes);
+		console.log( 'An error happened: ', error );
 
-// points geometry
-const pointsMaterial = new PointsMaterial({
-	color: 'red',
-	size: 0.02, // in world units
-});
-
-const sunPoints = new Points(geometry, pointsMaterial);
-scene.add(sunPoints);
-
-
-// wireframe geometry
-const wireframeMaterial = new LineBasicMaterial({
-    color: 'green',
-    linewidth: 2
-});
-const wireframeGeometry = new WireframeGeometry(geometry);
-const earthWireframe = new LineSegments(wireframeGeometry,wireframeMaterial);
-earthWireframe.scale.set(.2, .2, .2);
-earthWireframe.position.x += 2;
-sunMesh.add(earthWireframe);
-
-
-// edges geometry
-const edgesMaterial = new LineBasicMaterial({
-    color: 'blue',
-    linewidth: 2
-});
-const edgesGeometry = new EdgesGeometry(geometry);
-const moonEdges = new LineSegments(edgesGeometry,edgesMaterial);
-moonEdges.scale.set(.4, .4, .4);
-moonEdges.position.x += 1;
-earthMesh.add(moonEdges);
+	}
+);
 
 // 3 The camera
 
 const camera = new PerspectiveCamera(75, canvas.clientWidth/canvas.clientHeight);
 scene.add(camera);
-camera.position.x = 6;
-camera.position.y = 4;
-camera.position.z = 8;
+
 camera.lookAt(axes.position);
 
 // 4 The renderer
@@ -147,15 +97,9 @@ renderer.setClearColor(0x333333, 1);
 const light1 = new DirectionalLight();
 light1.position.set(3, 2, 1).normalize();
 scene.add(light1);
-// const light2 = new DirectionalLight();
-// light2.position.set(-3, 2, -1).normalize();
-// scene.add(light2);
 
 const ambientLight = new AmbientLight(0xffffff, .2);
 scene.add(ambientLight);
-
-// const hemisphereLight = new HemisphereLight(0xffffff, 0x7075ff, .2);
-// scene.add(hemisphereLight);
 
 // 6 Responsivity
 
@@ -190,6 +134,8 @@ const cameraControls = new CameraControls(camera, canvas);
 
 cameraControls.dollyToCursor = true;
 
+cameraControls.setLookAt(18, 20, 18, 0, 10, 0);
+
 
 // 8 Animation
 
@@ -197,9 +143,9 @@ function animate() {
     const delta = clock.getDelta();
 	cameraControls.update( delta );
     
-    sunMesh.rotation.y += .01;
-    earthMesh.rotation.y += .05;
-    sunPoints.rotation .y += .01;
+    // sunMesh.rotation.y += .01;
+    // earthMesh.rotation.y += .05;
+    // sunPoints.rotation .y += .01;
     
 	renderer.render( scene, camera );
     requestAnimationFrame(animate);
@@ -211,55 +157,55 @@ animate();
 // 9 Debugging
 
 
-const gui = new GUI();
+// const gui = new GUI();
 
-const min = -3;
-const max = 3;
-const step = .01;
+// const min = -3;
+// const max = 3;
+// const step = .01;
 
-const transformationFolder = gui.addFolder('Transformation');
+// const transformationFolder = gui.addFolder('Transformation');
 
-transformationFolder.add(sunMesh.position, 'x', min, max, step).name('Sun Position X');
-transformationFolder.add(sunMesh.position, 'y', min, max, step).name('Sun Position Y');
-transformationFolder.add(sunMesh.position, 'z', min, max, step).name('Sun Position Z');
+// transformationFolder.add(sunMesh.position, 'x', min, max, step).name('Sun Position X');
+// transformationFolder.add(sunMesh.position, 'y', min, max, step).name('Sun Position Y');
+// transformationFolder.add(sunMesh.position, 'z', min, max, step).name('Sun Position Z');
 
 // transformationFolder.close();
 
-const sunReset = {
-    reset: () => {
-        gsap.to(sunMesh.position, { x: sunMesh.position.x = 0, duration: .5});
-        gsap.to(sunMesh.position, { y: sunMesh.position.y = 0, duration: .5});
-        gsap.to(sunMesh.position, { z: sunMesh.position.z = 0, duration: .5});
-    }
-}
+// const sunReset = {
+//     reset: () => {
+//         gsap.to(sunMesh.position, { x: sunMesh.position.x = 0, duration: .5});
+//         gsap.to(sunMesh.position, { y: sunMesh.position.y = 0, duration: .5});
+//         gsap.to(sunMesh.position, { z: sunMesh.position.z = 0, duration: .5});
+//     }
+// }
 
-gui.add(sunReset, 'reset').name('Reset Sun Position');
+// gui.add(sunReset, 'reset').name('Reset Sun Position');
 
 
 // gui.addFolder('Sun Visibility').close().add(sunMesh, 'visible');
 // gui.addFolder('Earth Visibility').close().add(earthMesh, 'visible');
 // gui.addFolder('Moon Visibility').close().add(moonMesh, 'visible');
 
-const visibilityFolder = gui.addFolder('Visibility');
+// const visibilityFolder = gui.addFolder('Visibility');
 
-visibilityFolder.add(sunMesh, 'visible').name('Sun');
-visibilityFolder.add(earthMesh, 'visible').name('Earth');
-visibilityFolder.add(moonMesh, 'visible').name('Moon');
+// visibilityFolder.add(sunMesh, 'visible').name('Sun');
+// visibilityFolder.add(earthMesh, 'visible').name('Earth');
+// visibilityFolder.add(moonMesh, 'visible').name('Moon');
 
 // visibilityFolder.close();
 
-const colorParam = {
-    value: 0xffff00
-}
+// const colorParam = {
+//     value: 0xffff00
+// }
 
-gui.addColor(colorParam, 'value').name('Sun Color').onChange(() => {
-    sunMesh.material.color.set(colorParam.value);
-})
+// gui.addColor(colorParam, 'value').name('Sun Color').onChange(() => {
+//     sunMesh.material.color.set(colorParam.value);
+// })
 
-const functionParam = {
-    spin: () => {
-        gsap.to(earthMesh.rotation, { z: earthMesh.rotation.z + (Math.PI), duration: 1});
-    }
-}
+// const functionParam = {
+//     spin: () => {
+//         gsap.to(earthMesh.rotation, { z: earthMesh.rotation.z + (Math.PI), duration: 1});
+//     }
+// }
 
-gui.add(functionParam, 'spin').name('Earth Z Spin');
+// gui.add(functionParam, 'spin').name('Earth Z Spin');
